5ed96c42e2b854ce48aab318fc062d646641ad7a
Philipp <ufedz@student.kit.edu>
2018-06-24 20:01:35 +0200

    Implemented Feed Forward Test Case, but was unable to run it (Error: module unit not found)

--- src/model/mlp_tests.py
+++ src/model/mlp_tests.py
@@ -12,6 +12,31 @@ class MlpTests(unittest.TestCase):
         multilayer_perceptron = MultilayerPerceptron()
         self.assertEqual(1, 2)
 
+    def test_feed_forward(self):
+        train = []
+        for i in range(127):
+            train.append(1)
+
+        valid = []
+        test = []
+        layers = None
+        inputWeights = None
+        outputTask = 'classification'
+        outputActivation = 'softmax'
+        loss = 'bce'
+        learningRate = 0.01
+        epochs = 50
+        mlp = MultilayerPerceptron(train, valid, test, layers,
+                                                      inputWeights, outputTask, outputActivation,
+                                                      loss, learningRate, epochs)
+        res = mlp._feed_forward(train)
+        print(res)
+
+        """only placeholder, i couldn't run the testcase"""
+        exp_res = [1,2,3,4]
+        self.assertItemsEqual(res, exp_res)
+
+
 
 if __name__ == '__main__':
     unittest.main()
